f2b55603a1c92ae7ead1ca2eca52eeccaf8b0237
ARJUN PRABHAKARAN <ARJUN PRABHAKARAN@192.168.1.9>
2021-06-17 16:58:16 +0530

    pushing to a branch added my change

---
+++ SampleMaven/src/main/java/pages/TestClass.java
@@ -0,0 +1,10 @@
+package pages;
+
+public class TestClass 
+{
+    public static void main(String[] args)
+    {
+    	System.out.println("Added a new class");
+    	System.out.println("Test branch and merge with master");
+    }
+}
